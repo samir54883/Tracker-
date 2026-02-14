0b46d652f299bb02654a89728c588ed9efa20720
Samir Singh <samir54883@gmail.com>
2026-02-14 13:09:28 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Wrapper = styled.div`
 `;
 
 const ArrowButton = styled(Button)`
-    padding: 6px 4px;   /* smaller horizontal padding */
+    padding: 6px 5px;   /* smaller horizontal padding */
 `;
 
 
